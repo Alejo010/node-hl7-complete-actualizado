6f1f5fd1e4ae3f5f486a44489602adab0f0f949a
Alejandro <123392613+Alejo010@users.noreply.github.com>
2024-04-19 05:05:49 -0400

    Update guia-asignar.js

--- guia-asignar.js
+++ guia-asignar.js
@@ -1,74 +1,48 @@
 const NodeHL7Complete = require('./index');
+
+// Crear una instancia del módulo
 const hl7Converter = new NodeHL7Complete();
 
-//Funcion para formato de fecha y hora para la cadena hl7v2
-function formatHL7DateLocal(date) {
-    return date.getFullYear() +
-           ('0' + (date.getMonth() + 1)).slice(-2) +
-           ('0' + date.getDate()).slice(-2) +
-           ('0' + date.getHours()).slice(-2) +
-           ('0' + date.getMinutes()).slice(-2) +
-           ('0' + date.getSeconds()).slice(-2);
-  }
+// Mensaje HL7 de ejemplo
+const hl7Message = 'MSH|^~\\&|Webind|Neonatologa y Discapacidad Infantil|Webind|Neonatologa y Discapacidad Infantil|20240419033727||ADT^A01|123456|P|2.5\r';
 
-  
-// Utilizar la función para asignar la información del ACK y MSH respectivamente
-// Objeto JSON de ejemplo con la estructura de un mensaje ACK HL7
-  var Datos = {
-    nombreSisEmisor: 'Webind',
-    nombreSisReceptor: 'Webind',
-    nombreConsultaEmisora: 'Neonatología y Discapacidad Infantil',
-    nombreConsultaReceptora: 'Neonatología y Discapacidad Infantil',
-    id: '123456',
-};
-
-function Asignar(Datos) {
-const jsData = {
-  ACK: {
-    MSH: {
-      'MSH.1': '|',
-      'MSH.2': '^~\\&',
-      'MSH.3': {
-        'HD.1': Datos.nombreSisEmisor   //Nombre del sistema emisor
-      },
-      'MSH.4': {
-        'HD.1': Datos.nombreConsultaEmisora
-      },
-      'MSH.5': {
-        'HD.1': Datos.nombreSisReceptor
-      },
-      'MSH.6': {
-        'HD.1': Datos.nombreConsultaReceptora
-      },
-      'MSH.7': {
-        'TS.1': formatHL7DateLocal(new Date())
-      },
-      'MSH.9': {
-        'MSG.1': 'ACK',
-      },
-      'MSH.10': Datos.id, // Este debería ser el ID de control de mensaje original (MSH.10 del mensaje recibido)
-      'MSH.11': 'P',
-      'MSH.12': '2.5',
-    },
-    MSA: {
-      'MSA.1': 'AA', // Indica que el mensaje fue aceptado
-      'MSA.2': Datos.id, // Este debería ser el mismo ID de control de mensaje original (MSH.10 del mensaje recibido)
-    }
-  }
-   
-};
-return jsData;   
+// Función para manejar los datos asignados
+function handleDatosAsignados(jsObject) {
+  let Datos = {
+    nombreSisEmisor: jsObject.ADT_A01.MSH[0]['MSH.3'][0]['HD.1'][0],
+    nombreSisReceptor: jsObject.ADT_A01.MSH[0]['MSH.5'][0]['HD.1'][0],
+    nombreConsultaEmisora: jsObject.ADT_A01.MSH[0]['MSH.4'][0]['HD.1'][0],
+    nombreConsultaReceptora: jsObject.ADT_A01.MSH[0]['MSH.6'][0]['HD.1'][0],
+    id: jsObject.ADT_A01.MSH[0]['MSH.10'][0]
+  };
+  // Mostrar los datos asignados
+  console.log('Datos asignados:', Datos);
+  return Datos;
 }
-var jsData = Asignar(Datos);
 
-// Convertir el objeto JavaScript a un mensaje HL7
-hl7Converter.jsToHl7('ACK', jsData, (error, hl7Message) => {
+// Convertir mensaje HL7 a objeto JavaScript
+hl7Converter.hl7ToJs(hl7Message, (error, jsObject) => {
   if (error) {
-    console.error('Error al convertir objeto JavaScript a mensaje HL7:', error);
+    console.error('Error al convertir mensaje HL7 a objeto JavaScript:', error);
   } else {
-    console.log('Objeto JavaScript convertido a mensaje HL7:');
-    console.log(hl7Message.split('\r').join('\n'));
+    console.log('Mensaje HL7 convertido a objeto JavaScript:', jsObject);
+    // Agregar este console.log para ver el objeto JSON devuelto
+    console.log('Objeto JSON devuelto:', JSON.stringify(jsObject, null, 2));
+    // Obtener los datos asignados utilizando una promesa
+    const datosPromesa = new Promise((resolve) => {
+      resolve(handleDatosAsignados(jsObject));
+    });
+    datosPromesa.then((datos) => {
+      console.log('Datos retornados:', datos);
+    });
   }
-  
- });
+});
 
+
+module.exports = { handleDatosAsignados };
+
+//Utilizar la función handleDatosAsignados en otros archivos 
+
+//const { handleDatosAsignados } = require('./nombreDelArchivo.js');
+
+// Luego puedes utilizar la función handleDatosAsignados en este archivo
